af8002e0865399aa1d50f704838d14c8bf78c6fb
deepthi308 <deepthih6@gmail.com>
2023-05-04 11:06:43 +0530

    Pig Game

--- script.js
+++ script.js
@@ -10,44 +10,69 @@ const result2 = document.querySelector('.result2');
 const modal = document.querySelector('.win-modal');
 const win = document.querySelector('.winner');
 const quit = document.querySelector('.quit');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
 
 let count = 1;
 let player1Score = 0;
 let player2Score = 0;
 let winner;
-
+left.classList.add('active');
 // Creating functionalities
 function handleDiceRoll() {
-
+    // debugger;
+// count = 1;
     dots.forEach((dot) => {
         if (dot.classList.contains('hidden')) {
             dot.classList.remove('hidden');
         }
     })
-    const randomNumber = Math.ceil(Math.random() * 6);
-    if (randomNumber === 1 && count%2 !== 0) {
-        current1.innerText = 0;
+    const randomNumber = Math.ceil(Math.random()*6);
+    // debugger;
+   
+    if (randomNumber > 1 && count === 1) {
+        if (right.classList.contains('active')) {
+            right.classList.remove('active');
+            left.classList.add('active');
+        }
+            current1.innerText = Number(current1.innerText) + randomNumber;
     }
-    if (randomNumber === 1 && count%2 === 0){
+    else if(randomNumber > 1 && count === 2) {
+            current2.innerText = Number(current2.innerText) + randomNumber;
+            if (left.classList.contains('active')) {
+        left.classList.remove('active');
+        right.classList.add('active'); 
+        }
+    }
+    if (randomNumber === 1 && count === 2){
         current2.innerText = 0;
+        count=1;
+        if (right.classList.contains('active')) {
+            right.classList.remove('active');
+            left.classList.add('active');
+        }
+        
     }
-    if (randomNumber > 1) {
-        if (count % 2 !== 0) {
-        current1.innerText = Number(current1.innerText)+randomNumber;
-    } else {
-        current2.innerText = Number(current2.innerText)+randomNumber;;
+     else if (randomNumber === 1 && count === 1) {
+        current1.innerText = 0;
+        count = 2;
+        if (left.classList.contains('active')) {
+        left.classList.remove('active');
+        right.classList.add('active'); 
+        }
+        
     }
-    }
+    
     const hiddenDots = 6 - randomNumber;
     for (let i = 1; i <= hiddenDots; i++){
         dots[i].classList.add('hidden');
     }
-    count++;
-    console.log(hiddenDots, randomNumber);
+    // count++;
+    console.log(count);
 }
 
 const handleHold = function () {
-    if (count % 2 === 0) {
+    if (count === 1) {
         player1Score = Number(result1.innerText);
         player1Score = Number(current1.innerText) + player1Score;
         result1.innerText = player1Score;
@@ -59,7 +84,7 @@ const handleHold = function () {
         }
         
     }
-    if (count % 2 !== 0) {
+    if (count  === 2) {
         player2Score = Number(result2.innerText);
         player2Score = Number(current2.innerText) + player2Score;
         result2.innerText = player2Score;
